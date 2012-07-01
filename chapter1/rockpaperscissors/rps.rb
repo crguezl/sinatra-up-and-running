c8459897b8da7c68dae17421fed63370aa0867c1
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2012-07-01 12:04:48 +0100

    version using haml

--- chapter1/rockpaperscissors/rps.rb
+++ chapter1/rockpaperscissors/rps.rb
@@ -1,27 +1,40 @@
 require 'sinatra'
+require 'haml'
 
 # before we process a route we'll set the response as plain text
 # and set up an array of viable moves that a player (and the
 # computer) can perform
 before do
-  content_type :txt
   @defeat = { rock: :scissors, paper: :rock, scissors: :paper}
   @throws = @defeat.keys
 end
 
 get '/throw/:type' do
   # the params hash stores querystring and form data
-  player_throw = params[:type].to_sym
+  @player_throw = params[:type].to_sym
 
-  halt(403, "You must throw one of the following: '#{@throws.join(', ')}'") unless @throws.include? player_throw
+  halt(403, "You must throw one of the following: '#{@throws.join(', ')}'") unless @throws.include? @player_throw
 
-  computer_throw = @throws.sample
+  @computer_throw = @throws.sample
 
-  if player_throw == computer_throw 
-    "Computer chooses: #{computer_throw}. There is a tie"
-  elsif player_throw = @defeat[computer_throw]
-    "Computer wins; #{computer_throw} defeats #{player_throw}"
+  if @player_throw == @computer_throw 
+    @answer = "There is a tie"
+    haml :index
+  elsif @player_throw = @defeat[@computer_throw]
+    @answer = "Computer wins; #{@computer_throw} defeats #{@player_throw}"
+    haml :index
   else
-    "Well done. #{player_throw} beats #{computer_throw}"
+    @answer = "Well done. #{@player_throw} beats #{@computer_throw}"
+    haml :index
   end
 end
+__END__
+
+@@index
+%html
+  %head
+    %title Rock Paper Scissors
+  %body
+    %h2 Computer chooses:  #{@computer_throw}
+    %h2 You choose: #{@player_throw}
+    %h1= @answer
