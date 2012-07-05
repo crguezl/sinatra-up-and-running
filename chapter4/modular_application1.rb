f31dc5f6b7bf02077613bbfd171543324fac422b
Casiano Rodriguez <casiano.rodriguez.leon@gmail.com>
2012-07-05 12:11:07 +0100

    pages 67--70

--- chapter4/modular_application1.rb
+++ chapter4/modular_application1.rb
@@ -1,7 +1,10 @@
 # page 70, Examples 4-3 and 4-4
+# run:
+#   ruby modular_application1.rb
+# and open the browser at localhost:4567
 require "sinatra/base"
 
-class MyApp < sinatra::Base
+class MyApp < Sinatra::Base
   get '/' do
     "Hello from MyApp!"
   end
