f8104bd4db1606396f0ea8604e5169ae56b14492
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2012-07-10 12:50:31 +0100

    Playing with rack. https://github.com/rack/rack/blob/master/example/lobster.ru

--- chapter3/request_and_response/rackexample/rack/lobster.rb
+++ chapter3/request_and_response/rackexample/rack/lobster.rb
@@ -7,54 +7,54 @@ module Rack
   # Paste has a Pony, Rack has a Lobster!
   class Lobster
     LobsterString = Zlib::Inflate.inflate("eJx9kEEOwyAMBO99xd7MAcytUhPlJyj2
-    P6jy9i4k9EQyGAnBarEXeCBqSkntNXsi/ZCvC48zGQoZKikGrFMZvgS5ZHd+aGWVuWwhVF0
-    t1drVmiR42HcWNz5w3QanT+2gIvTVCiE1lm1Y0eU4JGmIIbaKwextKn8rvW+p5PIwFl8ZWJ
+    P6jy9i4k9EQyGAnBarEXeCBqSkntNXsi/
+    ZCvC48zGQoZKikGrFMZvgS5ZHd+aGWVuWwhVF0
+    t1drVmiR42HcWNz5w3QanT+2gIvTVCi
+    E1lm1Y0eU4JGmIIbaKwextKn8rvW+p5PIwFl8ZWJ
     I8jyiTlhTcYXkekJAzTyYN6E08A+dk8voBkAVTJQ==".delete("\n ").unpack("m*")[0])
 
-    LambdaLobster = lambda { |env|
-      if env["QUERY_STRING"].include?("flip")
-        lobster = LobsterString.split("\n").
-          map { |line| line.ljust(42).reverse }.
-          join("\n")
-        href = "?"
-      else
-        lobster = LobsterString
-        href = "?flip"
-      end
-
-      content = ["<title>Lobstericious!</title>",
-                 "<pre>", lobster, "</pre>",
-                 "<a href='#{href}'>flip!</a>"]
-      length = content.inject(0) { |a,e| a+e.size }.to_s
-      [200, {"Content-Type" => "text/html", "Content-Length" => length}, content]
-    }
+    puts LobsterString
 
     def call(env)
+      puts "env:\n#{env.inspect}\n\n"
       req = Request.new(env)
+      puts "request:\n#{req.inspect}\n\n"
       if req.GET["flip"] == "left"
         lobster = LobsterString.split("\n").
           map { |line| line.ljust(42).reverse }.
           join("\n")
+        puts lobster
         href = "?flip=right"
       elsif req.GET["flip"] == "crash"
         raise "Lobster crashed"
-      else
+      elsif req.GET["flip"] == "right"
         lobster = LobsterString
         href = "?flip=left"
+      else
+        raise "Invalid option: Lobster crashed"
       end
 
       res = Response.new
-      res.write "<title>Lobstericious!</title>"
-      res.write "<pre>"
-      res.write lobster
-      res.write "</pre>"
-      res.write "<p><a href='#{href}'>flip!</a></p>"
-      res.write "<p><a href='?flip=crash'>crash!</a></p>"
+      res.write drawlobster(lobster, href)
       res.finish
+
+      puts "response:\n#{res.inspect}\n\n"
+
+      res
     end
 
-  end
-end
+    def drawlobster(lobster, href)
+      <<-"LOBSTER"
+        <title>Lobstericious!</title>
+        <pre>
+#{lobster}
+        </pre>
+        <p><a href='#{href}'>flip!</a></p>
+        <p><a href='?flip=crash'>crash!</a></p>
+        LOBSTER
+    end
+  end # class Lobster
+end   # module Rack
 
 if $0 == __FILE__
   require 'rack'
