2173a25ad3d1e46a1999f951a56ac1c8f1cea299
Casiano Rodriguez-Leon <casiano.rodriguez.leon@gmail.com>
2012-07-11 20:15:33 +0100

    Sinatra and Middleware. Running the example at page 62

--- chapter3/rack_it_up/sinatra_and_middleware_example3_15_62.rb
+++ chapter3/rack_it_up/sinatra_and_middleware_example3_15_62.rb
@@ -1,8 +1,23 @@
 require 'sinatra'
 require 'rack'
 
+# Rack::Runtime  is a middleware that ships with Rack 
+# and sets the X-Runtime header.
+# Rack::Runtime calculates the time the request took
+# to process.
 use Rack::Runtime
 
 get('/') do
   '"Hello world!"'
 end
+
+# [19:56][~/src/ruby/sinatra/sinatraupandrunning/chapter3/rack_it_up(master)]$ curl -I 0.0.0.0:4567
+# HTTP/1.1 200 OK
+# X-Frame-Options: sameorigin
+# X-XSS-Protection: 1; mode=block
+# Content-Type: text/html;charset=utf-8
+# Content-Length: 14
+# X-Runtime: 0.027618
+# Connection: keep-alive
+# Server: thin 1.3.1 codename Triple Espresso
+
