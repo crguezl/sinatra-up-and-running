db39582782b6f95ee9532073297660e4b25a1623
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-11-19 13:12:32 +0000

    playing with Sinatra/Stream from sinatra-contrib

--- chapter2/streaming/simple.rb
+++ chapter2/streaming/simple.rb
@@ -5,12 +5,20 @@ require 'sinatra/streaming'
 set server: 'thin'
 #set server: 'unicorn'
 
+before do
+  content_type :txt
+end
+
 get '/' do
   stream do |out|
     puts out.class
     out.puts "Hello World!", "How are you?"
+    sleep 1
     out.write "Written #{out.pos} bytes so far!\n"
+    sleep 1
     out.putc(65) unless out.closed?
+    sleep 1
+    out.puts "\nEnd of the Story"
     out.flush
   end
 end
